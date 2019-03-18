20aeb89f7d7409c5ed2aa81fa3e318d80333ccf8
SandraTsao <s_tsao@fanshaweonline.ca>
2019-03-18 16:01:06 -0400

    animated banner

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -5,22 +5,47 @@
 	//grabs the shields at the bottom of the page
 	const	shields	= document.querySelectorAll(".sigil-container"),
 		 	lightBox = document.querySelector(".lightbox"),
-		 	video	= document.querySelector("video");
+		 	video	= document.querySelector("video"),
+		 	exit = document.querySelector(".lightbox-close"),
+		 	banners = document.querySelector("#houseImages");
 
 	function showLightbox() {
-		lightBox.classList.add("show-lightbox");
+		//grab right video source
+		// debugger;
+		//get the lowercase house name from the class list
+		let targetHouse = this.className.split(" ")[1];
 
+		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		video.src = `video/House-${targetSrc}.mp4`;
+
+
+		lightBox.classList.add("show-lightbox");
+		video.load();
+		video.play();
 	}
 
-	function hideLightbox() {
+	function closeLightbox() {
 		lightBox.classList.remove("show-lightbox");
 		video.currentTime = 0;
 		video.pause();
 	}
 
-	shields.forEach(shield => shield.addEventListener("click", showLightbox));
+	function animateBanner() {
+		//this is width of one img
+		const offSet = 600;
+		//total distance we need to move for each shield click on
+		totalOffset = this.dataset.offset * offSet + "px";
+		// set the style (css animate for us)
+		banners.style.right =totalOffset;
+	}
 
-	video.addEventListener("ended", hideLightbox);
+	// shields.forEach(shield => shield.addEventListener("click", showLightbox));
+	shields.forEach(shield => shield.addEventListener("click", animateBanner));
+	
+	
+	video.addEventListener("ended", closeLightbox);
+	exit.addEventListener("click", closeLightbox);
 
 
 })();
